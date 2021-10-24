bb11a76122adcf48e8c2bea0efcb6cfdaf4d8d59
Hoang <hoang1482002@gmail.com>
2021-10-24 21:33:54 +0700

    24/10

--- pages/api/review/[id].js
+++ pages/api/review/[id].js
@@ -1,12 +1,12 @@
 import nextConnect from "next-connect";
 import dbConnect from "../../../config/dbConnect";
-import { getALlReviews } from "../../../controllers/reviewController";
-import isAuthenticated from "../../../middlewares/auth";
+import { getRoomReviews } from "../../../controllers/reviewController";
+import { isAuthenticated } from "../../../middlewares/auth";
 import globalErrorHandler from "../../../middlewares/error";
 
 const handler = nextConnect({ onError: globalErrorHandler });
 
 dbConnect();
 
-handler.use(isAuthenticated).get(getALlReviews);
+handler.use(isAuthenticated).get(getRoomReviews);
 export default handler;
