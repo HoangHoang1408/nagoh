70cf8305d953cd1e9096d478ab2369e3d540aa19
Hoang <hoang1482002@gmail.com>
2021-10-24 22:33:24 +0700

    oke1

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,16 +2,14 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
   const transport = nodemailer.createTransport({
-    // host: process.env.SMTP_HOST,
-    // port: process.env.SMTP_PORT,
     service: "gmail",
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
+      user: "clashofclansphamviethoang@gmail.com",
+      pass: "hoang1482002",
     },
   });
   const message = {
-    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+    from: "Nagoh noreply@nagoh.com",
     to: options.email,
     subject: options.subject,
     text: options.message,
