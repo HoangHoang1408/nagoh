6bdff7ed1fb997b95017695c19578aaa3754a403
Hoang <hoang1482002@gmail.com>
2021-11-14 22:32:55 +0700

    oke

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -7,14 +7,23 @@ import {
 } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyButGmLABh8aSNYasU9PbROUTYFPbs28NE",
+//   authDomain: "bookit-9a9e7.firebaseapp.com",
+//   projectId: "bookit-9a9e7",
+//   storageBucket: "bookit-9a9e7.appspot.com",
+//   messagingSenderId: "494465565116",
+//   appId: "1:494465565116:web:3dd8c12ea7429c75946bb9",
+//   measurementId: "G-MPV074TW98",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyButGmLABh8aSNYasU9PbROUTYFPbs28NE",
-  authDomain: "bookit-9a9e7.firebaseapp.com",
-  projectId: "bookit-9a9e7",
-  storageBucket: "bookit-9a9e7.appspot.com",
-  messagingSenderId: "494465565116",
-  appId: "1:494465565116:web:3dd8c12ea7429c75946bb9",
-  measurementId: "G-MPV074TW98",
+  apiKey: "AIzaSyAkWNzhYRIE6WSaxUP9tz1uJjNNQDiD_uw",
+  authDomain: "bookit-46dd0.firebaseapp.com",
+  projectId: "bookit-46dd0",
+  storageBucket: "bookit-46dd0.appspot.com",
+  messagingSenderId: "682746419280",
+  appId: "1:682746419280:web:4a68e3032fb3638b689955",
+  measurementId: "G-ML84HXL1DE",
 };
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
